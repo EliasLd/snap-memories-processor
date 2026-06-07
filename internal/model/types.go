687d0d38af92caa24f6c5046d2f49b1e040ed7bf
EliasLd <area_causation872@simplelogin.fr>
2026-06-07 15:54:15 +0200

    feat(types): add central types for Media and Metadata

---
+++ internal/model/types.go
@@ -0,0 +1,31 @@
+package model
+
+import "time"
+
+type Metadata struct {
+	Date time.Time
+
+	Latitude  float64
+	Longitude float64
+
+	MediaType string
+}
+
+type Media struct {
+	MainPath string
+
+	OverlayPath string
+
+	HasOverlay bool
+
+	Metadata Metadata
+}
+
+type Job struct {
+	Media Media
+}
+
+type Result struct {
+	File string
+	Err  error
+}
